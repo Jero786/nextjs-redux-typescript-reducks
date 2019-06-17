99a4fc0ed5fe9e1731db3355f3cb508719e7c5d9
Jero <jero.carrizo@gmail.com>
2019-06-16 22:57:55 -0300

    master - fix some connection jest

--- setupTests.ts
+++ setupTests.ts
@@ -5,9 +5,6 @@ import jsdom from 'jsdom';
 
 enzyme.configure({ adapter: new Adapter() });
 
-const _ref = typeof window !== 'undefined' ? window : global;
-const { setTimeout } = _ref;
-const { now } = _ref.Date;
 // const dom = new jsdom.JSDOM('<!doctype html><html><body></body></html>')
 const dom = new jsdom.JSDOM('<html><body><script></script></body></html>');
 global.document = dom.window.document;
